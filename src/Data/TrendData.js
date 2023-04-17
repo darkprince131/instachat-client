8620f23b23c16c6064e4e6cdc5611b1ad1c4e129
darkprince131 <piladebajyoti131@gmail.com>
2023-04-17 19:41:06 +0530

    first commit

---
+++ src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "John Wick",
+    shares: 97,
+  },
+  {
+    name: "Avengers",
+    shares: 80.5,
+  },
+  {
+    name: "Eminem",
+    shares: 75.5,
+  },
+  {
+    name: "Evil Dead",
+    shares: 72,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "KR$NA",
+    shares: 20,
+  },
+];
